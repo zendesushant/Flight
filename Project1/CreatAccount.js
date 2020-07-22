b5d87b7d883d3b779b9c1664336129eb597875bf
zendesushant <sushantcz99@gmail.com>
2020-07-22 13:02:03 +0530

    New Files Uploded

---
+++ Project1/CreatAccount.js
@@ -0,0 +1,15 @@
+
+
+
+
+function validate()
+{
+    
+    var Name=document.querySelector(".name").value;
+    var para=document.createElement('p');
+    para.appendChild(document.createTextNode(Name));
+    document.body.appendChild(para);
+ 
+}
+
+
